5dfdf718f7a950b2cb8cd2e862f885fc9ed61d9a
Mukherji, Sandeep <sandeep.talk@gmail.com>
2024-11-30 12:04:43 +0530

    main: Prmove unwanted fiiles

--- bff/src/main.ts
+++ bff/src/main.ts
@@ -1,5 +1,5 @@
 import { bootstrap } from './bootstrap';
 import 'source-map-support/register';
 
-const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000;
+const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 4000;
 bootstrap(port);
